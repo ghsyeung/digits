c02b1a5c736cc4315f97e40724f1741cd26ccf26
Gary Yeung <gary@yeung>
2015-07-14 19:41:10 -0700

    Adding Neural Network with 50 nodes in the hidden layer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-import { neuralNetworkGuess } from './nn';
+import { NN30, NN50 } from './nn';
 
 (function() {
   var $ = function(id) {
@@ -60,8 +60,12 @@ import { neuralNetworkGuess } from './nn';
       return x / 255.0; 
     });
 
-    result_vec = neuralNetworkGuess(normalized_bitarr);
+    result_vec = NN30.neuralNetworkGuess(normalized_bitarr);
     console.log(result_vec);
+
+    result_vec = NN50.neuralNetworkGuess(normalized_bitarr);
+    console.log(result_vec);
+
   };
 
   canvas.setHeight(112);
--- app/scripts/nn.js
+++ app/scripts/nn.js
@@ -1,33 +1,50 @@
 import { vecAdd, vecDot, sigmoid } from './helpers';
-let weights, biases;
+export let NN30, NN50;
 
 (function() {
   $.getJSON("data/weights.json", function(w) {
     if (w) {
-      weights = w;
-      console.log(weights);
       $.getJSON("data/biases.json", function(b) {
         if (b) {
-          biases = b;
-          console.log(biases);
+          NN30 = new NeuralNetwork(w, b);
         }
       });
     }
   });
 })();
 
-export function neuralNetworkGuess(bitarr) {
-  var npl, prevLevelResult = [], levelResult = [];
-  prevLevelResult = bitarr;
-  for (var i = 0; i < weights.length; i++) {
-    npl = weights[i].length;
-    levelResult = [];
-    for (var j = 0; j < npl; j++) {
-      levelResult.push(sigmoid(vecDot(prevLevelResult, weights[i][j]) + biases[i][j][0]));
+(function() {
+  $.getJSON("data/weights_50.json", function(w) {
+    if (w) {
+      $.getJSON("data/biases_50.json", function(b) {
+        if (b) {
+          NN50 = new NeuralNetwork(w, b);
+        }
+      });
     }
-    prevLevelResult = levelResult;
+  });
+})();
+
+export class NeuralNetwork {
+  constructor(weights, biases) {
+    this.weights = weights;
+    this.biases = biases;
   }
-  return levelResult;
-};
+  neuralNetworkGuess(bitarr) {
+    let npl, prevLevelResult = [], levelResult = [];
+    let weights = this.weights, biases = this.biases;
+
+    prevLevelResult = bitarr;
+    for (var i = 0; i < weights.length; i++) {
+      npl = weights[i].length;
+      levelResult = [];
+      for (var j = 0; j < npl; j++) {
+        levelResult.push(sigmoid(vecDot(prevLevelResult, weights[i][j]) + biases[i][j][0]));
+      }
+      prevLevelResult = levelResult;
+    }
+    return levelResult;
+  };
+}
 
 
